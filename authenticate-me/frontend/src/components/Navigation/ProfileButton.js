602b5f1ee480058f33921b202762a902c7dbde17
dtannyc1 <dtannyc1@gmail.com>
2023-05-24 17:08:01 -0400

    added dropdown menu

--- authenticate-me/frontend/src/components/Navigation/ProfileButton.js
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { logoutUser } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -8,15 +8,39 @@ const ProfileButton = props => {
     const currentUser = useSelector(state => state.session.user);
     const [showMenu, setShowMenu] = useState(false);
 
+    const openMenu = () => {
+        console.log("opened")
+        setShowMenu(true)
+    }
+
+    useEffect(() => {
+        const closeMenu = () => {
+            console.log("closed")
+            setShowMenu(false)
+        }
+
+        if (showMenu){
+            window.addEventListener("click", closeMenu);
+
+            return function cleanup() {
+                window.removeEventListener("click", closeMenu);
+            }
+        }
+    }, [showMenu])
+
     return (
         <div className="profile-button">
-            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-user" onClick={e => openMenu()}></i>
 
-            <p>{currentUser.username}</p>
-            <button onClick={ async e => {
-                e.preventDefault();
-                dispatch(logoutUser(currentUser.id))
-            }}>Log Out!</button>
+            {(showMenu) ? <div className="profile-dropdown">
+                <p>{currentUser.username}</p>
+                <p>{currentUser.email}</p>
+                <button onClick={ async e => {
+                    e.preventDefault();
+                    dispatch(logoutUser(currentUser.id))
+                }}>Log Out!</button>
+                </div> : <></>
+            }
         </div>
     )
 }
