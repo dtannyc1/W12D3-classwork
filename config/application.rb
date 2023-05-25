7400420ec59d13079a868f5934da282617f97714
dtannyc1 <dtannyc1@gmail.com>
2023-05-25 09:48:17 -0400

    renamed entire project

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AuthenticateMe
+module BenchBnB
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -36,7 +36,7 @@ module AuthenticateMe
     # Skip views, helpers and assets when generating a new resource.
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
-        key: '_auth_me_session',
+        key: '_bench_bnb_session',
         same_site: :lax,
         secure: Rails.env.production?
     config.api_only = true
